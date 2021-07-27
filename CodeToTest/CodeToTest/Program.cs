8b1de8107892b3f71bc1b683a5060697dc0eede4
swase <fg2g15@soton.ac.uk>
2021-07-27 13:59:14 +0100

    Add Unit Test

--- CodeToTest/CodeToTest/Program.cs
+++ CodeToTest/CodeToTest/Program.cs
@@ -6,7 +6,26 @@ namespace CodeToTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Greeting(21);
+
+        }
+
+        private static string Greeting(int timeOfDay)
+        {
+            string greeting;
+            if (timeOfDay >= 5 && timeOfDay <= 12)
+            {
+                greeting = "Good Morning!";
+            }
+            else if (timeOfDay > 12 && timeOfDay <= 18)
+            {
+                greeting = "Good Afternoon!";
+            }
+            else
+            {
+                greeting = "Good Evening!";
+            }
+            return greeting;
         }
     }
 }
